3ebba638d9e19c09dee4c33c16bde91a42d650fc
Garrett Mills <garrett@geekfi.gq>
2016-03-29 13:46:21 +0000

    added boot conf

---
+++ boot.s
@@ -0,0 +1,24 @@
+.set ALIGN,    1<<0             
+.set MEMINFO,  1<<1             
+.set FLAGS,    ALIGN | MEMINFO  
+.set MAGIC,    0x1BADB002       
+.set CHECKSUM, -(MAGIC + FLAGS) 
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+.section .bootstrap_stack, "aw", @nobits
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+.section .text
+.global _start
+.type _start, @function
+_start:
+	call kernel_main
+	cli
+	hlt
+.Lhang:
+	jmp .Lhang
+.size _start, . - _start
